8cb7d014590ab9e9d4938e9ebdbc2c5814ba8030
sudee7p <142159373+sudee7p@users.noreply.github.com>
2025-07-14 11:25:34 +0530

    Add files via upload

---
+++ q3/serviceA/serviceA/src/main/java/com/example/serviceA/util/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.example.serviceA.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
